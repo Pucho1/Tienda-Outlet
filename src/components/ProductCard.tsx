8a3526cea2f7475a061d48e1c53b45d8732f4090
Pucho <martinezochandarenam@gmail.com>
2025-12-06 17:01:43 +0100

    refactor: change card layout

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,6 +1,7 @@
 import { Product } from '../interfaces/product';
-// import GalleryImage from './galleryImage/GalleryImage';
 import Swipper from './swipper/Swipper';
+
+import './pruductCard.css'
 
 interface ProductCardProps {
   product: Product;
@@ -26,11 +27,12 @@ export const ProductCard = ({ product, onClick }: ProductCardProps) => {
         <Swipper images={product.images}/>
       </div>
 
-     {oferta && <div className="flex items-center bg-red-500 absolute z-10 bottom-40 sm:bottom-35 left-[13%] sm:left-[5%] w-15 pl-2 h-6">
+     {oferta && <div className="flex items-center bg-red-500 absolute z-10 bottom-34 sm:bottom-35 left-[6%] sm:left-[5%] w-15 pl-2 h-6 custom-banner-offer">
           <p className='text-sm text-white font-semibold'>Oferta</p>
       </div>}
 
-      <div className="p-8 sm:p-5 md:p-4 flex-col flex justify-between">
+      {/* INFO */}
+      <div className="p-4 sm:p-5 md:p-4 flex-col flex justify-between info-card">
 
         {/* NAME */}
         <h3 className="font-bold text-gray-800 multiline-ellipsis">{product.name}</h3>
