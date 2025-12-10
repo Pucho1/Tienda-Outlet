f132316ed66aee4780e00cd9fccd041c7457eaba
Pucho <martinezochandarenam@gmail.com>
2025-12-10 11:06:03 +0100

    fix: change category type

--- src/service/CategoriesService.tsx
+++ src/service/CategoriesService.tsx
@@ -1,12 +1,12 @@
 import { AxiosResponse } from "axios";
 import newAxs from "../api/axiosSimplyCall";
 
-import { Categories } from "../interfaces/categories";
+import { Category } from "../interfaces/categories";
 
 const CategoriesService = () => {
 
-  const getCategoriesList = (): Promise<AxiosResponse<Categories[]>> => {
-		return newAxs.get<Categories[]>(`/category`);
+  const getCategoriesList = (): Promise<AxiosResponse<Category[]>> => {
+		return newAxs.get<Category[]>(`/category`);
 	};
 
   return{ getCategoriesList };
